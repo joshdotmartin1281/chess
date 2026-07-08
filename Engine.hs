7fc7028622bbd3da48bf47bcfc3af425bc3f98ab
joshdotmartin1281 <josh.martin1281@gmail.com>
2026-07-08 18:02:30 -0400

    feat (engine): Added movement around the board, instance of Show for board and an intial board for the starting positions. Next is creating Move types and validating that these are legal moves before moving onto PGN parser.

--- Engine.hs
+++ Engine.hs
@@ -21,11 +21,28 @@ data Square = Square
     }
     deriving (Eq, Ord, Show)
 
-newtype Board = Board (Map.Map Square Piece)
---    deriving (show)
+data Move = Move
+    { movingPiece :: PieceType
+    , destination :: Square
+    , capture      :: Bool
+    , promotion    :: Maybe PieceType
+    }
+    deriving (Show)
 
---instance Show Board where
---    show board = 
+newtype Board = Board (Map.Map Square Piece)
+
+instance Show Board where
+    show board = 
+        unlines $
+            "+ - + - + - + - + - + - + - + - +" :
+            concatMap renderRank ranks
+        where
+            files = [A, B, C, D, E, F, G, H]
+            ranks = [R8, R7, R6, R5, R4, R3, R2, R1]
+            renderRank r = 
+                [ "|" ++ concatMap (\f -> " " ++ [squareChar board (Square f r)] ++ " |") files 
+                    , "+ - + - + - + - + - + - + - + - +"
+                ]
 
 emptyBoard :: Board
 emptyBoard = Board Map.empty
@@ -35,6 +52,12 @@ placePiece sq piece (Board b) = Board (Map.insert sq piece b)
 
 removePiece :: Square -> Board -> Board
 removePiece sq (Board b) = Board (Map.delete sq b)
+
+movePiece :: Square -> Square -> Board -> Board
+movePiece start end board =
+    case pieceAt start board of
+        Nothing -> board
+        Just piece -> placePiece end piece (removePiece start board)
 
 pieceAt :: Square -> Board -> Maybe Piece
 pieceAt sq (Board b) = Map.lookup sq b
@@ -57,5 +80,47 @@ pieceChar (Piece Black King)   = 'k'
 squareChar :: Board -> Square -> Char
 squareChar board sq =
     case pieceAt sq board of
-        Nothing -> '.'
+        Nothing -> '*'
         Just p  -> pieceChar p
+
+initialBoard :: Board
+initialBoard = foldr (\(sq, p) b -> placePiece sq p b) emptyBoard pieces
+  where
+    pieces =
+        [ (Square A R1, Piece White Rook)
+        , (Square B R1, Piece White Knight)
+        , (Square C R1, Piece White Bishop)
+        , (Square D R1, Piece White Queen)
+        , (Square E R1, Piece White King)
+        , (Square F R1, Piece White Bishop)
+        , (Square G R1, Piece White Knight)
+        , (Square H R1, Piece White Rook)
+
+        , (Square A R2, Piece White Pawn)
+        , (Square B R2, Piece White Pawn)
+        , (Square C R2, Piece White Pawn)
+        , (Square D R2, Piece White Pawn)
+        , (Square E R2, Piece White Pawn)
+        , (Square F R2, Piece White Pawn)
+        , (Square G R2, Piece White Pawn)
+        , (Square H R2, Piece White Pawn)
+
+        , (Square A R7, Piece Black Pawn)
+        , (Square B R7, Piece Black Pawn)
+        , (Square C R7, Piece Black Pawn)
+        , (Square D R7, Piece Black Pawn)
+        , (Square E R7, Piece Black Pawn)
+        , (Square F R7, Piece Black Pawn)
+        , (Square G R7, Piece Black Pawn)
+        , (Square H R7, Piece Black Pawn)
+
+        , (Square A R8, Piece Black Rook)
+        , (Square B R8, Piece Black Knight)
+        , (Square C R8, Piece Black Bishop)
+        , (Square D R8, Piece Black King)
+        , (Square E R8, Piece Black Queen) 
+        , (Square F R8, Piece Black Bishop)
+        , (Square G R8, Piece Black Knight)
+        , (Square H R8, Piece Black Rook)
+        ]
+
